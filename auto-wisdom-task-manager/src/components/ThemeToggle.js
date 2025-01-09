a17fe6cd83c6895ca73ddc7970a23a53d230bc47
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2025-01-10 00:04:03 +0530

    Create ThemeToggle.js

---
+++ auto-wisdom-task-manager/src/components/ThemeToggle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAdjust } from '@fortawesome/free-solid-svg-icons';
+
+const ThemeToggle = ({ toggleTheme }) => {
+  return (
+    <button className="theme-toggle" onClick={toggleTheme}>
+  <FontAwesomeIcon icon={faAdjust} />
+</button>
+  );
+};
+
+export default ThemeToggle;
