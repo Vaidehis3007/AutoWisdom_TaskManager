de32ebe029802ecd12556ffec254ac0c70625d94
Vaidehis3007 <126815536+Vaidehis3007@users.noreply.github.com>
2025-01-10 00:04:11 +0530

    Create App.js

---
+++ auto-wisdom-task-manager/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AutoWisdomTaskManager from "./components/AutoWisdomTaskManager";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <AutoWisdomTaskManager />
+      </main>
+    </div>
+  );
+}
+
+export default App;
